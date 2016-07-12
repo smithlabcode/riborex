e27221fdd069d1f32bdc8b205effee66fd75f6f4
Wenzheng Li <wenzhenl@usc.edu>
2016-07-12 15:30:16 -0700

    add support of arbitray experiment design to edgeR and Voom, tested with 2 condition

--- R/riborex.r
+++ R/riborex.r
@@ -17,6 +17,36 @@
 # along with this software. If not, see
 # <http://www.gnu.org/licenses/>.
 
+combineDesignMatrix <- function (rnaCond, riboCond)
+{
+    if (!is.data.frame(rnaCond)) rnaCond <- data.frame(cond = rnaCond)
+    if (!is.data.frame(riboCond)) riboCond <- data.frame(cond = riboCond)
+
+    if (ncol(rnaCond) != ncol(riboCond))
+        stop("rna-seq and ribo-seq must have the same number of conditions")
+
+    numCond <- ncol(rnaCond)
+    numRNASmps <- nrow(rnaCond)
+    numRiboSmps <- nrow(riboCond)
+
+    ### expand rna covariate vector with 0s
+    rnaExpansion <- matrix(factor(rep(rep(0,numCond+1), numRNASmps)), nrow=numRNASmps)
+    rnaCond <- cbind(rnaCond, as.data.frame(rnaExpansion))
+    numExtendedCols <- length(colnames(rnaCond))
+    colnames(rnaCond)[(numCond+1):numExtendedCols] <- paste0("extra",seq(numCond+1))
+
+    ### expand ribo covariate vector by repeating the same vector
+    riboCond <- cbind(riboCond, intercept=factor(1), riboCond)
+    colnames(riboCond)[(numCond+1):numExtendedCols] <- paste0("extra",seq(numCond+1))
+
+    ### combine rna and ribo design matrix
+    combinedCond <- rbind(rnaCond, riboCond)
+
+    extendedConds <- paste0("combinedCond$", colnames(combinedCond))
+    fmla <- as.formula(paste("~", paste(extendedConds, collapse= "+")))
+    model.matrix(fmla)
+}
+
 DESeq2Rex <- function (rnaCntTable, riboCntTable, rnaCond, riboCond)
 {
 
@@ -58,74 +88,39 @@ DESeq2Rex <- function (rnaCntTable, riboCntTable, rnaCond, riboCond)
 
 edgeRRex <- function (rnaCntTable, riboCntTable, rnaCond, riboCond)
 {
-    library(edgeR)
-
-    ## combine design matrix
-    ## order of data: control RNA samples, case RNA samples,
-    ##                control Ribo samples, case Ribo samples
-    condition <- factor(c(rep(0, numCtlRNASmps), rep(1, numCaseRNASmps),
-                          rep(0, numCtlRiboSmps), rep(1, numCaseRiboSmps)))
-    dataType <- factor(c(rep(0, ncol(rnaCntTable)), rep(1, ncol(riboCntTable))))
-    RiboDiff <- factor(c(rep(0, ncol(rnaCntTable) + numCtlRiboSmps),
-                         rep(1, numCaseRiboSmps)))
-
     ## combine counts
     combCntTbl <- cbind(rnaCntTable, riboCntTable)
 
     dge <- DGEList(counts = combCntTbl)
     dge <- calcNormFactors(dge)
-    design <- model.matrix(~condition+dataType+RiboDiff)
+    design <- combineDesignMatrix(rnaCond, riboCond)
     dge <- estimateDisp(dge, design)
 
     ## glmFit and glmLRT
     fit <- glmFit(dge, design)
     lrt <- glmLRT(fit)
     topGenes <- topTags(lrt, n=Inf)
+    topGenes
 }
 
 voomRex <- function (rnaCntTable, riboCntTable, rnaCond, riboCond)
 {
-    library(edgeR)
-
-    numCaseRNASmps <- ncol(rnaCntTable) - numCtlRNASmps
-
-    group1 <- factor(c(rep(1,numCtlRNASmps), rep(2,numCaseRNASmps)))
-
-    design1 <- model.matrix(~group1)
-
-    numCaseRiboSmps <- ncol(riboCntTable) - numCtlRiboSmps
-
-    group2 <- factor(c(rep(1,numCtlRiboSmps), rep(2,numCaseRiboSmps)))
-
-    design2 <- model.matrix(~group2)
-
-    ## combine design matrix
-    ## order of data: control RNA samples, case RNA samples,
-    ##                control Ribo samples, case Ribo samples
-    condition <- factor(c(rep(0, numCtlRNASmps), rep(1, numCaseRNASmps),
-                          rep(0, numCtlRiboSmps), rep(1, numCaseRiboSmps)))
-    dataType <- factor(c(rep(0, ncol(RNACntTable)), rep(1, ncol(RiboCntTable))))
-    RiboDiff <- factor(c(rep(0, ncol(RNACntTable) + numCtlRiboSmps),
-                         rep(1, numCaseRiboSmps)))
-
     ## combine counts
-    combCntTbl <- cbind(RNACntTable, RiboCntTable)
+    combCntTbl <- cbind(rnaCntTable, riboCntTable)
 
     dge <- DGEList(counts = combCntTbl)
     dge <- calcNormFactors(dge)
-    design <- model.matrix(~condition+dataType+RiboDiff)
+    design <- combineDesignMatrix(rnaCond, riboCond)
     v <- voom(dge, design, plot=FALSE)
     fit <- lmFit(v, design)
     fit <- eBayes(fit)
 
     topGenes <- topTable(fit, coef=ncol(design), number=Inf)
+    topGenes
 }
 
 riborex <- function (rnaCntTable, riboCntTable, rnaCond, riboCond, engine)
 {
-    if(missing(engine)) {
-      message("No engine selected, DESeq2 will be used by default")
-    }
     if (engine == "DESeq2") {
         DESeq2Rex(rnaCntTable, riboCntTable, rnaCond, riboCond)
     }
